a89d0b2ca8203de26c3a27c64277da7908498b1e
Bellmena04 <belenmenacho04@gmail.com>
2024-07-17 22:24:49 +0000

    modified: ejercicios/Color.java

--- ejercicios/Color.java
+++ ejercicios/Color.java
@@ -1,14 +1,14 @@
 public class Color {
-    // Enumeración de colores
+
     public enum Colores {
         Rojo, Verde, Morado
     }
 
     public static void main(String[] args) {
-        // Uso del enum para almacenar un color favorito
+       
         Colores miColorFavorito = Colores.Morado;
 
-        // Imprimir el color favorito
+       
         System.out.println("Mi color favorito es " + miColorFavorito);
     }
 }

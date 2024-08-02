af0cd70dd1d2a3db12f958d64e7a2fd1a45223d2
Bellmena04 <belenmenacho04@gmail.com>
2024-08-02 22:06:41 +0000

    new file: ejercicios/Calculadora.java

---
+++ ejercicios/Calculadora.java
@@ -0,0 +1,14 @@
+public class Calculadora {
+    public int sumar(int a, int b){
+        return a + b;
+    }
+
+    public double suma(double a, double b){
+        return a + b;
+
+    }
+
+    public int sumar(int a, int b, int c){
+        return a + b + c;
+    }
+}
